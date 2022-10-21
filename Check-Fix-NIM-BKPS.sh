1ec3b0120e78c875c60c2c38d7ca1fcbb4653466
juancarbone <juancarbone@gmail.com>
2022-10-21 17:54:19 -0300

    Check-Fix-NIM-BKPS-random.sh
    Check-Fix-NIM-BKPS.sh

--- Check-Fix-NIM-BKPS.sh
+++ Check-Fix-NIM-BKPS.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+hora=$(( 0 + $RANDOM % 9))
+echo $hora
 fecha=`date +%d``date +%m`
 hostname=`uname -n`
 hostname=${hostname:0:2}
@@ -9,7 +11,7 @@ then
         cp /usr/local/bin/mksysbaz* /usr/local/bin/mksysbaz11
         ls -la /usr/local/bin
         cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root_bkp_$fecha
-        echo "0 3 * * 1 /usr/local/bin/mksysbaz11 2>&1 #Remove comment and define proper AZ name for the
+        echo "0 $hora * * 1 /usr/local/bin/mksysbaz11 2>&1 #Remove comment and define proper AZ name for the
 script." >> /var/spool/cron/crontabs/root
         ./usr/local/bin/mksysbaz11 &
 elif [[ $hostname == g4 ]]
@@ -18,7 +20,7 @@ then
         cp /usr/local/bin/mksysbaz* /usr/local/bin/mksysbaz1
         ls -la /usr/local/bin
         cp /var/spool/cron/crontabs/root /var/spool/cron/crontabs/root_bkp_$fecha
-        echo "0 3 * * 1 /usr/local/bin/mksysbaz1 2>&1 #Remove comment and define proper AZ name for the
+        echo "0 $hora * * 1 /usr/local/bin/mksysbaz1 2>&1 #Remove comment and define proper AZ name for the
 script." >> /var/spool/cron/crontabs/root
         ./usr/local/bin/mksysbaz1 &
 fi
